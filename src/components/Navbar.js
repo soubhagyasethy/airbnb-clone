cca170ef3a0e0b864e5870e421864e1512b9749b
soubhagyasethy <ssethy017@gmail.com>
2023-01-20 00:14:35 +0530

    feat: added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react"
+import logo from "../images/airbnb-logo.png"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="./images/airbnb-logo.png" className="nav--logo" />
+            <img className="nav--logo" src={logo} alt="airbnb logo" />
         </nav>
     )
 }
