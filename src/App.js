12a9511bf84eacec85557b380e45fd9d60cd1b61
soubhagyasethy <ssethy017@gmail.com>
2023-01-24 02:25:19 +0530

    refactor: pass dat object into props

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ export default function App() {
   const cards = data.map((item) => {
     return(
       <Card 
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
+        item={item}
       />
     )
   })
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,14 @@ import React from "react"
 export default function Card(props) {
     return (
             <div className="card">
-                <img className="card-image" src={props.img} alt="katie zaferes" />
+                <img className="card-image" src={props.item.coverImg} alt="katie zaferes" />
                 <div className="card-rating">
                     <img className="card-star" src="../images/star.png" alt="star" />
-                    <p>{props.rating}&nbsp;<span style={{opacity:"0.6"}}>({props.reviewCount})&nbsp;• {props.location}</span> </p>
+                    <p>{props.item.stats.rating}&nbsp;<span style={{opacity:"0.6"}}>({props.item.stats.reviewCount})&nbsp;• {props.item.location}</span> </p>
                 </div>
                 <div className="card-text">
-                    <p>{props.title}</p>
-                    <p><b>From ${props.price}</b> / person</p>
+                    <p>{props.item.title}</p>
+                    <p><b>From ${props.item.price}</b> / person</p>
                 </div>
             </div>
         
