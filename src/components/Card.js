2320dcff0c693396c2af935b89e3cf5f4d695916
soubhagyasethy <ssethy017@gmail.com>
2023-01-24 02:14:38 +0530

    feat: added login button, updated card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,17 @@ import React from "react"
 
 export default function Card(props) {
     return (
-        <section className="card">
-            <img className="card-image" src={props.img} alt="katie zaferes" />
-        <div className="card-rating">
-            <img className="card-star" src="../images/star.png" alt="star" />
-            <p>{props.rating}&nbsp;<span style={{opacity:"0.6"}}>({props.reviewCount})&nbsp;• {props.location}</span> </p>
-        </div>
-        <div className="card-text">
-            <p>{props.title}</p>
-            <p><b>From ${props.price}</b> / person</p>
-        </div>
-        </section>
+            <div className="card">
+                <img className="card-image" src={props.img} alt="katie zaferes" />
+                <div className="card-rating">
+                    <img className="card-star" src="../images/star.png" alt="star" />
+                    <p>{props.rating}&nbsp;<span style={{opacity:"0.6"}}>({props.reviewCount})&nbsp;• {props.location}</span> </p>
+                </div>
+                <div className="card-text">
+                    <p>{props.title}</p>
+                    <p><b>From ${props.price}</b> / person</p>
+                </div>
+            </div>
         
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ export default function Navbar() {
     return (
         <nav>
             <img className="nav--logo" src="../images/airbnb-logo.png" alt="airbnb" />
+            <div className="login"><img className="login-image" src="../images/login.svg" alt="login-icon"/>Login</div>
         </nav>
     )
 }
