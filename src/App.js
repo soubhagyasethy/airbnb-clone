9f26fd239c2d9a0f062e2647e0a90c1efe13b9ed
soubhagyasethy <ssethy017@gmail.com>
2023-01-24 22:12:48 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-import './App.css';
+import './App.css'
 import data from "./data"
 
 
